42f209e2e05ec06dfd6ce5d2bb0a901677a9486f
Felix Lui <59271703+lmy031133@users.noreply.github.com>
2021-05-06 12:38:03 +0800

    Delete forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-
-
